800a79a1673a5ea768b4c1a5769ffe3a78e82e49
Muhammad Ferdiansyah <57201932+muhammadferdiansyah22@users.noreply.github.com>
2023-05-13 22:57:49 +0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -16,3 +16,31 @@ void printStack(stack <int> myStack){
 		cout << endl;
 	}
 }
+int main(){
+	stack<int> myStack;
+	
+	myStack.push(10);
+	myStack.push(20);
+	myStack.push(30); 
+	
+	printStack(myStack);
+	
+	cout << "Top element: " << myStack.top() << endl;
+	
+	myStack.pop();
+	cout << endl;
+	
+	printStack(myStack);
+	
+	cout << "Top element after pop: " << myStack.top() << endl;
+	
+	cout << "Stack size: " << myStack.size() << endl;
+	
+	//fungsi clear stack
+	while(!myStack.empty()){
+		myStack.pop();
+	}
+	
+	printStack(myStack);
+	return 0;
+}
