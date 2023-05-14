5ad36bbf5b1e1888f9f7f408a6c3b25929b2db29
Muhammad Ferdiansyah <57201932+muhammadferdiansyah22@users.noreply.github.com>
2023-05-14 23:36:47 +0700

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -16,3 +16,31 @@ void printQueue(queue <int> myQueue){
 		cout << endl;
 	}
 }
+int main(){
+	queue<int> myQueue;
+	
+	myQueue.push(10);
+	myQueue.push(20);
+	myQueue.push(30); 
+	
+	printQueue(myQueue);
+	
+	cout << "Front element: " << myQueue.front() << endl;
+	
+	myQueue.pop();
+	cout << endl;
+	
+	printQueue(myQueue);
+	
+	cout << "Front element after pop: " << myQueue.front() << endl;
+	
+	cout << "Queue size: " << myQueue.size() << endl;
+	
+	//fungsi clear queue
+	while(!myQueue.empty()){
+		myQueue.pop();
+	}
+	
+	printQueue(myQueue);
+	return 0;
+}
